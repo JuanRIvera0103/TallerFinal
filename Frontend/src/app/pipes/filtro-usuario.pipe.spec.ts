82af6e0823d6ac127a03e9e5d7efa463d6e07055
JuanRivera0103 <juanriverarestrepo0103@gmail.com>
2020-09-28 15:50:52 -0500

    Se elimino la carpeta front

--- Frontend/src/app/pipes/filtro-usuario.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltroUsuarioPipe } from './filtro-usuario.pipe';
-
-describe('FiltroUsuarioPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltroUsuarioPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
